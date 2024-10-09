9aafd10272c55e6fd8cd52edd199b683240822c1
lgc2 <lgustavoccarvalho@gmail.com>
2024-10-08 21:47:47 -0300

    feat(): implements requests models and create category handler

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,5 +1,9 @@
 using Dima.Api.Data;
+using Dima.Api.Handlers;
+using Dima.Core.Handlers;
 using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Responses;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,51 +20,16 @@ builder.Services.AddSwaggerGen(x =>
 {
 	x.CustomSchemaIds(n => n.FullName);
 });
-builder.Services.AddTransient<Handler>();
+builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapPost(
-	"/v1/categories",
-	(Request request, Handler handler) => handler.Handle(request))
+app.MapPost("/v1/categories", (CreateCategoryRequest request, ICategoryHandler handler) => handler.CreateAsync(request))
 	.WithName("Categories: Create")
 	.WithSummary("Create a new category")
-	.Produces<Response>();
+	.Produces<Response<Category>>();
 
 app.Run();
-
-public class Request
-{
-	public string Title { get; set; } = string.Empty;
-	public string Description { get; set; } = string.Empty;
-}
-
-public class Response
-{
-	public long Id { get; set; }
-	public string Title { get; set; } = string.Empty;
-}
-
-public class Handler(AppDbContext context)
-{
-	public Response Handle(Request request)
-	{
-		var category = new Category
-		{
-			Title = request.Title,
-			Description = request.Description
-		};
-
-		context.Categories.Add(category);
-		context.SaveChanges();
-
-		return new Response
-		{
-			Id = category.Id,
-			Title = category.Title
-		};
-	}
-}
