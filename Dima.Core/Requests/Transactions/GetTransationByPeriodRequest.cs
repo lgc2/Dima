40dc2a411a89a6cdcee402f1719dc9e360503d6e
lgc2 <lgustavoccarvalho@gmail.com>
2024-10-13 17:29:08 -0300

    feat(Transaction): implement Requests

---
+++ Dima.Core/Requests/Transactions/GetTransationByPeriodRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Dima.Core.Requests.Transactions;
+
+public class GetTransationByPeriodRequest : PagedRequest
+{
+	public DateTime? StartDate { get; set; }
+	public DateTime? EndDate { get; set; }
+}
---
+++ Dima.Core/Requests/Transactions/UpdateTransactionRequest.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Dima.Core.Enums;
+
+namespace Dima.Core.Requests.Transactions;
+
+public class UpdateTransactionRequest : Request
+{
+	public long Id { get; set; }
+
+	[Required(ErrorMessage = "Invalid title")]
+	public string Title { get; set; } = string.Empty;
+
+	[Required(ErrorMessage = "Invalid type")]
+	public ETransactionType Type { get; set; }
+
+	[Required(ErrorMessage = "Invalid amount")]
+	public decimal Amount { get; set; }
+
+	[Required(ErrorMessage = "Invalid category")]
+	public long CategoryId { get; set; }
+
+	[Required(ErrorMessage = "Invalid date")]
+	public DateTime? PaidOrReceivedAt { get; set; }
+}
