0d7335ff82bffdf576ff9a5c5792b8db089ef06d
lgc2 <lgustavoccarvalho@gmail.com>
2024-10-18 20:42:22 -0300

    chore(): add identity package and resolve packages vulnerabilities

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -2,14 +2,16 @@ using Dima.Api.Data;
 using Dima.Api.Endpoints;
 using Dima.Api.Handlers;
 using Dima.Core.Handlers;
-using Dima.Core.Models;
-using Dima.Core.Requests.Categories;
-using Dima.Core.Responses;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using static System.Net.Mime.MediaTypeNames;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x => { x.CustomSchemaIds(n => n.FullName); });
+
+builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme).AddIdentityCookies();
+builder.Services.AddAuthorization();
 
 var cnnStr = builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
 
@@ -18,11 +20,6 @@ builder.Services.AddDbContext<AppDbContext>(x =>
 	x.UseSqlServer(cnnStr);
 });
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(x =>
-{
-	x.CustomSchemaIds(n => n.FullName);
-});
 builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
 builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
 
