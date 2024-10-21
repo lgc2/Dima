021230ac9c039d2c7160295d785779261e857415
lgc2 <lgustavoccarvalho@gmail.com>
2024-10-21 20:47:37 -0300

    feat(Identity): apply identity migration and configure identity endpoints

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,6 +1,7 @@
 using Dima.Api.Data;
 using Dima.Api.Endpoints;
 using Dima.Api.Handlers;
+using Dima.Api.Models;
 using Dima.Core.Handlers;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -19,16 +20,27 @@ builder.Services.AddDbContext<AppDbContext>(x =>
 {
 	x.UseSqlServer(cnnStr);
 });
+builder.Services
+	.AddIdentityCore<User>()
+	.AddRoles<IdentityRole<long>>()
+	.AddEntityFrameworkStores<AppDbContext>()
+	.AddApiEndpoints();
 
 builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
 builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
 
 var app = builder.Build();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.MapGet("/", () => new { message = "OK" });
 app.MapEndpoints();
+app.MapGroup("v1/identity")
+	.WithTags("Identity")
+	.MapIdentityApi<User>();
 
 app.Run();
