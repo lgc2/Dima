ec66dd80f66df3174b02de6471e01dbd6deb46cb
lgc2 <lgustavoccarvalho@gmail.com>
2024-10-30 21:09:31 -0300

    feat(Configuration): complete configurations and api program clean up

--- Dima.Api/Common/Api/BuilderExtension.cs
+++ Dima.Api/Common/Api/BuilderExtension.cs
@@ -13,6 +13,8 @@ public static class BuilderExtension
 	public static void AddConfiguration(this WebApplicationBuilder builder)
 	{
 		Configuration.ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
+		Configuration.BackendUrl = builder.Configuration.GetValue<string>("BackendUrl") ?? string.Empty;
+		Configuration.FrontendUrl = builder.Configuration.GetValue<string>("FrontendUrl") ?? string.Empty;
 	}
 
 	public static void AddDocumentation(this WebApplicationBuilder builder)
@@ -42,6 +44,14 @@ public static class BuilderExtension
 
 	public static void AddCrossOrigin(this WebApplicationBuilder builder)
 	{
+		builder.Services.AddCors(options => options.AddPolicy(
+			ApiConfiguration.CorsPolicyName,
+			policy => policy
+			.WithOrigins([Configuration.BackendUrl, Configuration.FrontendUrl])
+			.AllowAnyMethod()
+			.AllowAnyHeader()
+			.AllowCredentials()
+			));
 	}
 
 	public static void AddServices(this WebApplicationBuilder builder)
--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,8 +1,6 @@
-using System.Security.Claims;
+using Dima.Api;
 using Dima.Api.Common.Api;
 using Dima.Api.Endpoints;
-using Dima.Api.Models;
-using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddConfiguration();
@@ -14,46 +12,11 @@ builder.AddServices();
 
 var app = builder.Build();
 
-app.UseAuthentication();
-app.UseAuthorization();
+if (app.Environment.IsDevelopment())
+	app.ConfigureDevEnvironment();
 
-app.UseSwagger();
-app.UseSwaggerUI();
-
-app.MapGet("/", () => new { message = "OK" });
+app.UseCors(ApiConfiguration.CorsPolicyName);
+app.UseSecurity();
 app.MapEndpoints();
-app.MapGroup("v1/identity")
-	.WithTags("Identity")
-	.MapIdentityApi<User>();
-
-app.MapGroup("v1/identity")
-	.WithTags("Identity")
-	.MapPost("/logout", async (SignInManager<User> signInManager) =>
-	{
-		await signInManager.SignOutAsync();
-		return Results.Ok();
-	})
-	.RequireAuthorization();
-
-app.MapGroup("v1/identity")
-	.WithTags("Identity")
-	.MapPost("/roles", (ClaimsPrincipal user) =>
-	{
-		if (user.Identity is null || !user.Identity.IsAuthenticated)
-			return Results.Unauthorized();
-
-		var identity = (ClaimsIdentity)user.Identity;
-		var roles = identity.FindAll(identity.RoleClaimType).Select(c => new
-		{
-			c.Issuer,
-			c.OriginalIssuer,
-			c.Type,
-			c.Value,
-			c.ValueType
-		});
-
-		return TypedResults.Json(roles);
-	})
-	.RequireAuthorization();
 
 app.Run();
