fa318311220e0ca72a6573e5db39b78decb154cf
lgc2 <lgustavoccarvalho@gmail.com>
2024-11-18 20:07:03 -0300

    feat(Identity): start identity implementation for web project

--- Dima.Web/Configuration.cs
+++ Dima.Web/Configuration.cs
@@ -4,6 +4,11 @@ namespace Dima.Web;
 
 public static class Configuration
 {
+	public static string BackendUrl { get; set; } = "http://localhost:5267";
+
+
+	public const string HttpClientName = "dima";
+
 	public static MudTheme Theme = new()
 	{
 		Typography = new()
