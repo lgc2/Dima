400693cbf87b6a3f355748aa1f80d0dfed69677f
lgc2 <lgustavoccarvalho@gmail.com>
2024-11-20 11:14:30 -0300

    feat(Identity): create login handler of web project

---
+++ Dima.Web/Handlers/AccountHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http.Json;
+using Dima.Core.Handlers;
+using Dima.Core.Requests.Account;
+using Dima.Core.Responses;
+
+namespace Dima.Web.Handlers;
+
+public class AccountHandler(IHttpClientFactory httpClientFactory) : IAccountHandler
+{
+	private readonly HttpClient _client = httpClientFactory.CreateClient(Configuration.HttpClientName);
+
+	public async Task<Response<string>> LoginAsync(LoginRequest request)
+	{
+		var result = await _client.PostAsJsonAsync("v1/identity/login?useCookies=true", request);
+		return result.IsSuccessStatusCode
+			? new Response<string>("Login realizado com sucesso", 200, "Login realizado com sucesso")
+			: new Response<string>(null, (int)result.StatusCode, "Não foi possível realizar login");
+	}
+
+	public Task<Response<string>> RegisterAsync(RegisterRequest request)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task LogoutAsync()
+	{
+		throw new NotImplementedException();
+	}
+}
