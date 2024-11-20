6aa81ad9ac56768c8cf563ef93ed2c255119d756
lgc2 <lgustavoccarvalho@gmail.com>
2024-11-20 18:06:40 -0300

    feat(Identity): create register and logout handlers of web project

--- Dima.Web/Handlers/AccountHandler.cs
+++ Dima.Web/Handlers/AccountHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http.Json;
+using System.Text;
 using Dima.Core.Handlers;
 using Dima.Core.Requests.Account;
 using Dima.Core.Responses;
@@ -17,13 +18,17 @@ public class AccountHandler(IHttpClientFactory httpClientFactory) : IAccountHand
 			: new Response<string>(null, (int)result.StatusCode, "Não foi possível realizar login");
 	}
 
-	public Task<Response<string>> RegisterAsync(RegisterRequest request)
+	public async Task<Response<string>> RegisterAsync(RegisterRequest request)
 	{
-		throw new NotImplementedException();
+		var result = await _client.PostAsJsonAsync("v1/identity/register", request);
+		return result.IsSuccessStatusCode
+			? new Response<string>("Cadastro realizado com sucesso", 201, "Cadastro realizado com sucesso")
+			: new Response<string>(null, (int)result.StatusCode, "Não foi possível realizar seu cadastro");
 	}
 
-	public Task LogoutAsync()
+	public async Task LogoutAsync()
 	{
-		throw new NotImplementedException();
+		var emptyContent = new StringContent("{}", Encoding.UTF8, "application/json");
+		await _client.PostAsJsonAsync("v1/identity/logout", emptyContent);
 	}
 }
