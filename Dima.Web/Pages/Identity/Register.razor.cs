c735277d8d3a0c7a7815746e08fc4a1f6f29252e
lgc2 <lgustavoccarvalho@gmail.com>
2024-12-07 11:53:34 -0300

    feat(Identity): add depedencie injection to Register Page

---
+++ Dima.Web/Pages/Identity/Register.razor.cs
@@ -0,0 +1,26 @@
+﻿using Dima.Core.Handlers;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using MudBlazor;
+
+namespace Dima.Web.Pages.Identity;
+
+public partial class RegisterPage : ComponentBase
+{
+	#region Depedencies
+	[Inject]
+	public ISnackbar Snackbar { get; set; } = null!;
+
+	[Inject]
+	public IAccountHandler Handler { get; set; } = null!;
+
+	[Inject]
+	public NavigationManager NavigationManager { get; set; } = null!;
+
+	[Inject]
+	public AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+
+	#endregion
+
+	//public MudForm? MudForm { get; set; }
+}
