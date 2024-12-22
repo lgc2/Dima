33f93c014b5e1aa18404ffc48ec78f1bfa3353aa
lgc2 <lgustavoccarvalho@gmail.com>
2024-12-22 17:14:21 -0300

    feat(Identity): implement login page redirect

--- Dima.Web/Pages/Identity/Login.razor.cs
+++ Dima.Web/Pages/Identity/Login.razor.cs
@@ -56,6 +56,8 @@ public partial class LoginPage : ComponentBase
 
 			if (result.IsSuccess)
 			{
+				await AuthenticationStateProvider.GetAuthenticationStateAsync();
+				AuthenticationStateProvider.NotifyAuthenticationStateChanged();
 				NavigationManager.NavigateTo("/");
 			}
 			else
