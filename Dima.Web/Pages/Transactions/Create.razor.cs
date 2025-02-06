76251af62c25887325f42bc7cb52e914bf972181
lgc2 <lgustavoccarvalho@gmail.com>
2025-02-06 20:44:52 -0300

    feat(Web): implement Create Transaction page

--- Dima.Web/Pages/Transactions/Create.razor.cs
+++ Dima.Web/Pages/Transactions/Create.razor.cs
@@ -1,6 +1,59 @@
-﻿namespace Dima.Web.Pages.Transactions
+﻿using Dima.Core.Handlers;
+using Dima.Core.Requests.Transactions;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Dima.Web.Pages.Transactions;
+
+public partial class CreateTransactionPage : ComponentBase
 {
-	public class CreateTransactionPage
+	#region Properties
+
+	public bool IsBusy { get; set; } = false;
+	public CreateTransactionRequest InputModel { get; set; } = new();
+
+	#endregion
+
+	#region Services
+
+	[Inject]
+	public ITransactionHandler Handler { get; set; } = null!;
+
+	[Inject]
+	public NavigationManager NavigationManager { get; set; } = null!;
+
+	[Inject]
+	public ISnackbar Snackbar { get; set; }
+
+	#endregion
+
+	#region Methods
+
+	public async Task OnValidSubmitAsync()
 	{
+		IsBusy = true;
+
+		try
+		{
+			var result = await Handler.CreateAsync(InputModel);
+			if (result.IsSuccess)
+			{
+				Snackbar.Add(result.Message, Severity.Success);
+				NavigationManager.NavigateTo("/lancamentos/historico");
+			}
+			else
+				Snackbar.Add(result.Message, Severity.Error);
+
+		}
+		catch (Exception ex)
+		{
+			Snackbar.Add(ex.Message, Severity.Error);
+		}
+		finally
+		{
+			IsBusy = false;
+		}
 	}
+
+	#endregion
 }
