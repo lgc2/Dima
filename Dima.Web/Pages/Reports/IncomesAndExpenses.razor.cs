da4192e712885fbf1bbbb6915e3a0f95a0995fe9
lgc2 <lgustavoccarvalho@gmail.com>
2025-05-10 14:42:32 -0300

    feat(Web): format the y-axis of the Income and Expenses chart to Brazilian currency

--- Dima.Web/Pages/Reports/IncomesAndExpenses.razor.cs
+++ Dima.Web/Pages/Reports/IncomesAndExpenses.razor.cs
@@ -1,4 +1,4 @@
-using System.Globalization;
+﻿using System.Globalization;
 using Dima.Core.Handlers;
 using Dima.Core.Requests.Reports;
 using Microsoft.AspNetCore.Components;
@@ -8,52 +8,57 @@ namespace Dima.Web.Pages.Reports;
 
 public partial class IncomesAndExpensesPage : ComponentBase
 {
-    #region Properties
+	#region Properties
 
-    protected List<ChartSeries> Series = new List<ChartSeries>()
-    {
-        new ChartSeries() { Name = "Receitas", Data = [] },
-        new ChartSeries() { Name = "Despesas", Data = [] },
-    };
+	protected ChartOptions ChartOptions = new()
+	{
+		YAxisFormat = "C2"
+	};
 
-    protected string[] XAxisLabels = [];
+	protected List<ChartSeries> Series = new List<ChartSeries>()
+	{
+		new ChartSeries() { Name = "Receitas", Data = [] },
+		new ChartSeries() { Name = "Despesas", Data = [] },
+	};
 
-    #endregion
-    
-    #region Services
+	protected string[] XAxisLabels = [];
 
-    [Inject]
-    public ISnackbar Snackbar { get; set; } = null!;
+	#endregion
 
-    [Inject]
-    public IReportHandler Handler { get; set; } = null!;
+	#region Services
 
-    #endregion
-    
-    protected override async Task OnInitializedAsync() => await GetIncomesAndExpensesAsync();
+	[Inject]
+	public ISnackbar Snackbar { get; set; } = null!;
 
-    private async Task GetIncomesAndExpensesAsync()
-    {
-        try
-        {
-            var result = await Handler.GetIncomesAndExpensesReportAsync(new GetIncomesAndExpansesRequest());
+	[Inject]
+	public IReportHandler Handler { get; set; } = null!;
 
-            if (!result.IsSuccess)
-                return;
-            
-            if (result.Data == null)
-                return;
+	#endregion
 
-            XAxisLabels = result.Data
-                .Select(x => CultureInfo.GetCultureInfo("pt-BR").DateTimeFormat.GetMonthName(x.Month))
-                .ToArray();
-            
-            Series[0].Data = result.Data.Select(x => (double)x.Incomes).ToArray();
-            Series[1].Data = result.Data.Select(x => (double)x.Expenses * -1).ToArray();
-        }
-        catch (Exception ex)
-        {
-            Snackbar.Add(ex.Message, Severity.Error);
-        }
-    }
+	protected override async Task OnInitializedAsync() => await GetIncomesAndExpensesAsync();
+
+	private async Task GetIncomesAndExpensesAsync()
+	{
+		try
+		{
+			var result = await Handler.GetIncomesAndExpensesReportAsync(new GetIncomesAndExpansesRequest());
+
+			if (!result.IsSuccess || result.Data is null)
+			{
+				Snackbar.Add("Falha ao obter o relatório", Severity.Error);
+				return;
+			}
+
+			XAxisLabels = result.Data
+				.Select(x => CultureInfo.GetCultureInfo("pt-BR").DateTimeFormat.GetMonthName(x.Month))
+				.ToArray();
+
+			Series[0].Data = result.Data.Select(x => (double)x.Incomes).ToArray();
+			Series[1].Data = result.Data.Select(x => (double)x.Expenses * -1).ToArray();
+		}
+		catch (Exception ex)
+		{
+			Snackbar.Add(ex.Message, Severity.Error);
+		}
+	}
 }
